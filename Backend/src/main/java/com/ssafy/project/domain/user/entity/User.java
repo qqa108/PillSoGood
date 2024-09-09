322654a7fc8009a43fc52c09a99863a29807c4de
정경원 <103038606+kw99j10@users.noreply.github.com>
2024-09-09 11:26:07 +0900

    init directory-2

---
+++ Backend/src/main/java/com/ssafy/project/domain/user/entity/User.java
@@ -0,0 +1,17 @@
+package com.ssafy.project.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+}
