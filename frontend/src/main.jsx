2ebeceb49f4d1db447ce43309911c823a6ff8e32
Leo Lee <ukal5006@naver.com>
2024-09-09 15:51:56 +0900

    init
    - 필요없는 파일들 삭제
    - router, recoil, styled-components 설치
    - router 기본 설정

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.js
+import ReactDOM from 'react-dom/client';
+import GlobalStyle from './assets/GlobalStyle.js';
+import { RouterProvider } from 'react-router-dom';
+import router from './router.jsx';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <>
+        <GlobalStyle />
+        <RecoilRoot>
+            <RouterProvider router={router} />
+        </RecoilRoot>
+    </>
+);
--- frontend/src/sw.js
+++ frontend/src/sw.js
@@ -1,25 +1,21 @@
 /// <reference lib="webworker" />
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { clientsClaim } from 'workbox-core'
-import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
+import { clientsClaim } from 'workbox-core';
+import { NavigationRoute, registerRoute } from 'workbox-routing';
 
 // self.__WB_MANIFEST is the default injection point
-precacheAndRoute(self.__WB_MANIFEST)
+precacheAndRoute(self.__WB_MANIFEST);
 
 // clean old assets
-cleanupOutdatedCaches()
+cleanupOutdatedCaches();
 
 /** @type {RegExp[] | undefined} */
-let allowlist
+let allowlist;
 // in dev mode, we disable precaching to avoid caching issues
-if (import.meta.env.DEV)
-  allowlist = [/^\/$/]
+if (import.meta.env.DEV) allowlist = [/^\/$/];
 
 // to allow work offline
-registerRoute(new NavigationRoute(
-  createHandlerBoundToURL('index.html'),
-  { allowlist },
-))
+registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html'), { allowlist }));
 
-self.skipWaiting()
-clientsClaim()
+self.skipWaiting();
+clientsClaim();
