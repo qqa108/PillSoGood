2bf68f7293242c22eb4d90c95256f35362e6b420
정경원 <ekdhfksl12@naver.com>
2024-09-16 15:26:32 +0900

    Revert "branch test"
    This reverts commit 9843c9edc0d00a76834ede4d8a905536985dd86e.

--- Backend/src/main/java/com/ssafy/project/global/config/WebConfig.java
+++ Backend/src/main/java/com/ssafy/project/global/config/WebConfig.java
@@ -1,30 +1,4 @@
-//package com.ssafy.project.global.config;
-//
-//import com.ssafy.back_end.oauth.presentation.OauthServerTypeConverter;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.format.FormatterRegistry;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOriginPatterns(
-//                        "https://i11b310.p.ssafy.io",
-//                        "http://i11b310.p.ssafy.io",
-//                        "http://localhost:3000"
-//                )
-//                .allowedMethods("*")
-//                .allowedHeaders("*")
-//                .allowCredentials(true)
-//                .maxAge(3600);
-//    }
-//
-//    @Override
-//    public void addFormatters(FormatterRegistry registry) {
-//        registry.addConverter(new OauthServerTypeConverter());
-//    }
-//}
+package com.ssafy.project.global.config;
+
+public class WebConfig {
+}
