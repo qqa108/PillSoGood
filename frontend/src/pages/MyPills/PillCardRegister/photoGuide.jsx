55c3c9e327939d9ce96afa5366f7fba713f4e317
sunmin <bbu0330@gmail.com>
2024-09-23 10:19:34 +0900

    feat: 약물 카드 등록 모달

---
+++ frontend/src/pages/MyPills/PillCardRegister/photoGuide.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function photoGuide() {
+  return (
+    <div>photoGuide</div>
+    // react-webcam 쓰기
+  )
+}
+
+
--- frontend/src/pages/MyPills/index.jsx
+++ frontend/src/pages/MyPills/index.jsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+import styled from "styled-components";
+import colors from '../../assets/colors';
+import { FaCirclePlus } from "react-icons/fa6";
+import PillCardRegister from "./PillCardRegister/RegisterModal";
+
+const IconContainer = styled.div`
+  width: 2rem;
+  height: 2rem;
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  position: fixed;
+  bottom: 5rem;
+  right: 1.2rem;
+  
+  & > svg {
+   font-size: 2rem;
+   color: ${colors.point2}
+  }
+
+`;
+
 function MyPills() {
-    return <>MyPills</>;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <>
+        My Pills
+        <IconContainer alt="Add Icon" onClick={openModal} >
+            <FaCirclePlus />
+        </IconContainer>
+
+        <PillCardRegister isModalOpen={isModalOpen} closeModal={closeModal} />
+        </>
+    );
 }
 
 export default MyPills;
