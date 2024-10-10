dc7e12ef5b500cafd720004e52876f11080c6e02
정경원 <ekdhfksl12@naver.com>
2024-10-10 12:56:46 +0900

    Revert "Merge branch 'deploy' into 'fe-develop'"
    This reverts commit 3a4f126cfa4788977b1a068eedee063c991f831c, reversing
    changes made to d0fb168800f30c499d215801a9e06c892034fcd4.

--- backend/src/main/java/com/ssafy/project/domain/userMedicationDetail/repository/UserMedicationDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.project.domain.userMedicationDetail.repository;
-
-import com.ssafy.project.domain.userMedicationDetail.entity.UserMedicationDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserMedicationDetailRepository extends JpaRepository<UserMedicationDetail, Integer> {
-}
